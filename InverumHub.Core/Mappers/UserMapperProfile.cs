ac153c05831df0a2dddfbf0376639a79e495cb5a
Jesus Romero <elherzt@gmail.com>
2026-01-13 16:12:55 -0600

    update mapper to include application roles

--- InverumHub.Core/Mappers/UserMapperProfile.cs
+++ InverumHub.Core/Mappers/UserMapperProfile.cs
@@ -11,7 +11,17 @@ namespace InverumHub.Core.Mappers
 {
     public class UserMapperProfile : Profile
     {
-        public UserMapperProfile() =>
-            CreateMap<User, UserDTO>();
+        public UserMapperProfile()
+        {
+            CreateMap<UserApplicationRole, UserApplicationRoleDTO>()
+                .ForMember(d => d.ApplicationName,
+                    o => o.MapFrom(s => s.Application.Name))
+                .ForMember(d => d.RoleName,
+                    o => o.MapFrom(s => s.Role.Name));
+
+            CreateMap<User, UserDTO>()
+                .ForMember(d => d.ApplicationsRoles,
+                    o => o.MapFrom(s => s.ApplicationRoles));
+        }
     }
 }
